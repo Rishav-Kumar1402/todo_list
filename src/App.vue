3c48efaee7c8b5a062874cd60981d5f097df3ad1
Rishav-Kumar1402 <122720572+Rishav-Kumar1402@users.noreply.github.com>
2023-09-26 00:17:40 +0530

    first commit

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+<div id="app">
+<todo-app>
+  
+</todo-app>
+</div>
+</template>
+
+<script>
+import TodoApp from './components/TodoApp.vue'
+
+export default {
+  name: 'App',
+  components: {
+    TodoApp
+  }
+}
+</script>
+
+<style>
+#app{
+  background-color: rgb(182, 238, 234);
+  min-height: 100vh;
+}
+</style>
